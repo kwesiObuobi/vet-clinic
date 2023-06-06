a4b46f7b99c93139d4505331d6f0cc67f7b13ab3
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-06 10:17:35 +0000

    Update Devimon's details

--- data.sql
+++ data.sql
@@ -5,4 +5,4 @@ VALUES
 ('Agumon', '2020-02-03', 10.23, true, 0),
 ('Gabumon', '2018-11-15', 8, true, 2),
 ('Pikachu', '2021-01-07', 15.04, false, 1),
-('Devimon', '2017-05-07', 11, true, 5);
+('Devimon', '2017-05-12', 11, true, 5);
