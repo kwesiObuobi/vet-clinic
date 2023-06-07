4f511c3d429f4fa3e5ac4277ed7001084fb6ac4a
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-07 09:40:54 +0000

    Set species to 'unspecified' and rollback

--- queries.sql
+++ queries.sql
@@ -8,3 +8,9 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutured is true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
