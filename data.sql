f130ad3582cbb47024262d154866e5b7d9f0983f
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-08 09:42:57 +0000

    Add 4 records to the 'specializations' table

--- data.sql
+++ data.sql
@@ -65,3 +65,10 @@ VALUES
 ('Maisy Smith', 26, '2019-01-17'),
 ('Stephanie Mendez', 64, '1981-05-04'),
 ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations
+VALUES
+(1, 1),
+(3, 1),
+(3, 2),
+(4, 2);
