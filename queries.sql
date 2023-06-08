0ce1d9d4b3e7fabf938234e99a83a37fdec22089
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-08 15:58:50 +0000

    Add queries to complete the required exercises

--- queries.sql
+++ queries.sql
@@ -79,3 +79,64 @@ ON o.id = a.owner_id
 WHERE o.id = (
 	SELECT owner_id FROM animals GROUP BY owner_id ORDER BY COUNT(*) DESC LIMIT 1
 ) GROUP BY o.full_name;
+
+
+/* 'JOIN TABLES' queries */
+SELECT v.name AS vet, a.name AS last_animal
+FROM vets v JOIN visits vs
+ON v.id = vs.vet_id
+JOIN animals a
+ON a.id = vs.animal_id
+WHERE v.id = 1 ORDER BY vs.date_of_visit DESC LIMIT 1;
+
+SELECT COUNT(DISTINCT animal_id) FROM visits
+WHERE vet_id = 3 GROUP BY vet_id;
+
+SELECT v.name AS vet, s.name AS specialty
+FROM vets v LEFT JOIN specializations spec
+ON v.id = spec.vet_id
+LEFT JOIN species s ON s.id = spec.species_id;
+
+SELECT v.name AS vet, a.name AS animal, vs.date_of_visit
+FROM vets v JOIN visits vs
+ON v.id = vs.vet_id
+JOIN animals a
+ON a.id = vs.animal_id
+WHERE (vs.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30')
+AND v.name = 'Stephanie Mendez';
+
+SELECT a.name, COUNT(*) AS most_visits
+FROM animals a JOIN visits vs
+ON a.id = vs.animal_id
+WHERE a.id = (
+	SELECT animal_id FROM visits GROUP BY animal_id ORDER BY COUNT(*) DESC LIMIT 1
+) GROUP BY a.name;
+
+SELECT a.name
+FROM animals a JOIN visits vs
+ON a.id = vs.animal_id
+WHERE vs.date_of_visit = (
+	SELECT date_of_visit FROM visits WHERE visits.vet_id = 2 ORDER BY date_of_visit LIMIT 1
+);
+
+SELECT * FROM animals
+FULL JOIN visits
+ON animals.id = visits.animal_id
+FULL JOIN vets
+ON vets.id = visits.vet_id
+WHERE visits.date_of_visit = (
+	SELECT MAX(date_of_visit) from visits
+);
+
+SELECT (COUNT(*) - 
+	( SELECT COUNT(*)
+	  FROM visits vs JOIN specializations spec ON vs.vet_id = spec.vet_id
+	  JOIN animals a ON a.species_id = spec.species_id AND vs.animal_id = a.id )
+) AS visits FROM visits;
+
+SELECT COUNT(*), s.name
+FROM visits vs JOIN animals a ON a.id = vs.animal_id
+JOIN species s ON a.species_id = s.id
+WHERE vs.vet_id = 2
+GROUP BY s.name
+ORDER BY COUNT(*) DESC LIMIT 1;
